92d6d61d7cc1c9c9b7c7d3091829f737ec3f3d11
FraDili95 <fradili95@gmail.com>
2024-02-27 23:33:52 +0100

    progetto completo

--- script.js
+++ script.js
@@ -37,48 +37,75 @@ changeBackColor(back);
 
 //ex9
 const address = document.querySelector("form");
-function replaceAddress(address) {
- address.firstElementChild.textContent = "Via delle filatrici 11";    
+function replaceAddress(address) {//funzione per sostituire l'indirizzo
+ address.firstElementChild.textContent = "Via delle filatrici 11";  //modifico il contenuto del testo del primo figlio   
 }
 replaceAddress(address);
+
 //ex10
 const addressTwo = document.getElementsByTagName("a");
 function addClassToA( tag ) {
     for ( let i = 0; i < tag.length; i++ ){
-        tag[i].classList.add("new_class");
+        tag[i].classList.add("new_class");//aggiunge la classe
     }
 }
 addClassToA(addressTwo);
 // ESERCIZIO 11: Scrivi una funzione per aggiungere/togliere una classe CSS
 // a tutte le immagini della tabella; questa classe deve modificare la visibilità/invisibilità dell'immagine
 
-const immagini = document.getElementsByTagName("img");
-const button = document.querySelector(".pulsante_poof");
+const immagini = document.getElementsByTagName("img");//puntatore a tutti elementi img
+const button = document.querySelector(".pulsante_poof");//puntatore a tutti elementi con classe pulsante_poof
 
-function addClassToImg( tag ) {
+function addClassToImg( tag ) {//aggiunge/toglie una classe ad ogni elemento dello speudoarray di img puntati 
    
     for ( let i = 0; i < tag.length; i++ ){
-        if( tag[i].classList.contains("poof") ){
-            tag[i].classList.remove("poof")
-        }else{
-            tag[i].classList.add("poof");
+
+        if( tag[i].classList.contains("poof") ){//se c'è
+            tag[i].classList.remove("poof")//la toglie
+        }else{//sennò
+            tag[i].classList.add("poof");//la aggiunge
         } 
     }
 
 }
 
 function desaparesito(immagini) {
-    addClassToImg(immagini);
+
+    addClassToImg(immagini);//aggiunge/toglie classe
     for ( let i = 0; i < immagini.length; i++ ){
-        if( immagini[i].classList.contains("poof")){
-            immagini[i].style.display = "none";
-        }else{
-            immagini[i].style.display = "block";
+        if( immagini[i].classList.contains("poof")){//se ha la classe
+            immagini[i].style.display = "none";//scompare
+        }else{//sennò
+            immagini[i].style.display = "block";//compare
         }
     }
    
 }
 
-button.addEventListener ("click", function() {
-    desaparesito(immagini);
+button.addEventListener ("click", function() {//ogni click corrisponde
+    desaparesito(immagini);// una chiamata a desaparesito che chiama a sua volta addclasstoimg ogni volta
 });
+
+// ESERCIZIO 12: Scrivi una funzione per cambiare il colore del prezzo di ogni prodotto in uno differente, ogni volta che viene invocata
+
+let parent = document.getElementsByTagName("tr");//puntatore ad ogni riga della tabella
+const parentSelection = [];//array costante vuoto
+for (let i = 1; i < parent.length; i++) {
+    parentSelection.push(parent[i]);  //pusho all'array vuoto l'indirizzo alle righe che mi servono(tutte tranne la prima)
+}
+function colorRandom() {//funzione generatrice di colori a caso
+    const MIN = 0; const MAX = 255;//estremi
+    return Math.floor( Math.random() * ( MAX - MIN + 1) ) +  MIN ;//ritorna a caso tra min e max
+}
+function pricesColorChange( ){//funzione che applica agli ultimi figli di ogni riga della tabella un rgb a caso
+    for (let i = 0; i < parentSelection.length; i++) {//per ogni last child
+        parentSelection[i].lastElementChild.style.color =  `rgb( ${ colorRandom() }, ${ colorRandom() }, ${ colorRandom() } )`; //applico 3 volte la funzione
+    }                                                     //uso `` per applicare la funzione all'interno del valore di rgb che altrimenti non prenderebbe
+}
+const buttonTwo = document.querySelector("#change_color");//punto il bottone
+buttonTwo.addEventListener( "click", pricesColorChange );
+
+   
+
+
+
